d2b53eeda47d4c55ec7fe40d394424852440663f
BirkanAk <bak1@binghamton.edu>
2021-02-16 23:41:22 +0300

    Hello Text Added, to do: Add a Button!

--- app/src/main/java/com/example/helloworldassignmenbirkanak/MainActivity.java
+++ app/src/main/java/com/example/helloworldassignmenbirkanak/MainActivity.java
@@ -3,13 +3,20 @@ package com.example.helloworldassignmenbirkanak;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView helloText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        helloText=(TextView)findViewById(R.id.hello_text);
+
+        //TODO Button.
     }
 }
 
