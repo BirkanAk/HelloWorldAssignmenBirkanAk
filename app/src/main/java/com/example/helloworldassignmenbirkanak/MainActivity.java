8a9535a6e38353106fd6a3a0a036ed2bb9941cd1
BirkanAk <bak1@binghamton.edu>
2021-02-17 20:10:37 +0300

    Epilepsy Button Added!

--- app/src/main/java/com/example/helloworldassignmenbirkanak/MainActivity.java
+++ app/src/main/java/com/example/helloworldassignmenbirkanak/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.helloworldassignmenbirkanak;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView helloText;
+    Button epilepsyButton;
+    View bg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +22,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         helloText=(TextView)findViewById(R.id.hello_text);
+        epilepsyButton=(Button)findViewById(R.id.epilepsy_button);
+        bg=(View)findViewById(R.id.rView);
 
-        //TODO Button.
+    }
+
+    public void onEpilepsyClick(View view){
+        Random rn = new Random();;
+        int randomNum =  rn.nextInt(68) + 1;
+        int red =  rn.nextInt(256);
+        int green =  rn.nextInt(256);
+        int blue =  rn.nextInt(256);
+        helloText.setTextSize(randomNum);
+        int red1 =  rn.nextInt(256);
+        int green1 =  rn.nextInt(256);
+        int blue1 =  rn.nextInt(256);
+        bg.setBackgroundColor(Color.rgb(red,green,blue));
+        epilepsyButton.setBackgroundColor(Color.rgb(red1,green1,blue1));
+
     }
 }
-
-//Github2
